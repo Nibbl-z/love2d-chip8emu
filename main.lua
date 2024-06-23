eaa5995623b98c80584767a95eecffb06efa8ca3
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-06-23 16:49:41 -0600

    started cpu

--- main.lua
+++ main.lua
@@ -29,6 +29,20 @@ local keymap = {
 local keysPressed = {}
 local onNextKeyPress = nil
 
+local memory = {} -- 4kb memory
+local v = {} -- registers
+local i = 0 -- stores memory address
+
+local delayTimer = 0
+local soundTimer = 0
+
+local pc = 0x200 -- program counter
+
+local stack = {}
+
+local paused = false
+local speed = 10
+
 function SetPixel(x, y)
     if x > cols then
         x = x - cols
@@ -58,11 +72,11 @@ function Clear()
 end
 
 function RenderDisplay()
-    for i = 1, cols * rows do
-        local x = (i % cols) * scale
-        local y = math.floor(i / cols) * scale
+    for index = 1, cols * rows do
+        local x = (index % cols) * scale
+        local y = math.floor(index / cols) * scale
 
-        if display[i] then
+        if display[index] then
             love.graphics.rectangle("fill", x - scale, y, scale, scale)
         end
     end
@@ -86,6 +100,125 @@ end
 function love.keyreleased(_, scancode)
     local key = keymap[scancode]
     keysPressed[key] = false
+end
+
+function LoadSpritesIntoMemory() 
+    local sprites = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, -- 0
+        0x20, 0x60, 0x20, 0x20, 0x70, -- 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, -- 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, -- 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, -- 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, -- 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, -- 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, -- 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, -- 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, -- 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, -- A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, -- B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, -- C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, -- D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, -- E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  -- F
+    }
+
+    for i = 1, #sprites do
+        memory[i] = sprites[i]
+    end
+end
+
+function LoadProgramIntoMemory(program)
+    for loc = 1, string.len(program) do
+        memory[0x200 + loc] = string.byte(program, loc, loc)
+    end
+end
+
+function LoadRom(romName)
+    local contents = love.filesystem.read("string", romName, math.huge)
+
+    LoadProgramIntoMemory(contents)
+end
+
+function ExecuteInstruction(opcode)
+    pc = pc + 2
+    
+    local x = bit.rshift(bit.band(opcode, 0x0F00), 8)
+    local y = bit.lshift(bit.band(opcode, 0x00F0), 4)
+
+    -- there is no swich statement in lua please forgive me
+
+    local instruction = bit.band(opcode, 0xF000)
+
+    if instruction == 0x000 then
+        if opcode == 0x00E0 then
+            Clear()
+        elseif opcode == 0x00EE then
+            pc = stack[#stack]
+            pc = table.remove(stack, #stack)
+        end
+    elseif instruction == 0x1000 then
+        pc = bit.band(opcode, 0xFFF)
+    elseif instruction == 0x2000 then
+        table.insert(stack, pc)
+        pc = bit.band(opcode, 0xFFF)
+    elseif instruction == 0x3000 then
+    elseif instruction == 0x4000 then
+    elseif instruction == 0x5000 then
+    elseif instruction == 0x6000 then
+    elseif instruction == 0x7000 then
+    elseif instruction == 0x8000 then
+        if bit.band(opcode, 0xF) == 0x0 then
+        elseif bit.band(opcode, 0xF) == 0x0 then
+        elseif bit.band(opcode, 0xF) == 0x1 then
+        elseif bit.band(opcode, 0xF) == 0x2 then
+        elseif bit.band(opcode, 0xF) == 0x3 then
+        elseif bit.band(opcode, 0xF) == 0x4 then
+        elseif bit.band(opcode, 0xF) == 0x5 then
+        elseif bit.band(opcode, 0xF) == 0x6 then
+        elseif bit.band(opcode, 0xF) == 0x7 then
+        elseif bit.band(opcode, 0xF) == 0xE then
+        end
+    elseif instruction == 0x9000 then
+    elseif instruction == 0xA000 then
+    elseif instruction == 0xB000 then
+    elseif instruction == 0xC000 then
+    elseif instruction == 0xD000 then
+    elseif instruction == 0xE000 then
+    elseif instruction == 0xF000 then
+        if bit.band(opcode, 0xFF) == 0x07 then
+        elseif bit.band(opcode, 0xFF) == 0x0A then
+        elseif bit.band(opcode, 0xFF) == 0x15 then
+        elseif bit.band(opcode, 0xFF) == 0x18 then
+        elseif bit.band(opcode, 0xFF) == 0x1E then
+        elseif bit.band(opcode, 0xFF) == 0x29 then
+        elseif bit.band(opcode, 0xFF) == 0x33 then
+        elseif bit.band(opcode, 0xFF) == 0x55 then
+        elseif bit.band(opcode, 0xFF) == 0x65 then
+        end
+    else
+        error("Unknown opcode: "..tostring(opcode))
+    end
+end
+
+function Cycle()
+    for i = 1, speed do
+        if not paused then
+            local opcode = bit.bor(bit.lshift(memory[pc], 8), memory[pc + 1])
+            
+        end
+    end
+
+    if not paused then
+        if delayTimer > 0 then
+            delayTimer = delayTimer - 1
+        end
+
+        if soundTimer > 0 then
+            soundTimer = soundTimer - 1
+        end
+    end
+
+    RenderDisplay()
 end
 
 function love.load()
