bdc22399e4d771aadf34686acf5b0bfe8b31599d
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-06-25 14:15:23 -0600

    starting over and making the code less of a mess: renderer added

---
+++ renderer.lua
@@ -0,0 +1,45 @@
+local renderer = {}
+
+renderer.cols = 64
+renderer.rows = 64
+renderer.scale = 10
+
+renderer.display = {}
+
+function renderer:InitializeDisplay()
+    for i = 1, self.cols * self.rows do
+        self.display[i] = false
+    end
+end
+
+function renderer:SetPixel(x, y)
+    if x > self.cols then
+        x = x - self.cols
+    elseif x < 0 then
+        x = x + self.cols
+    end
+
+    if y > self.rows then
+        y = y - self.rows
+    elseif y < 0 then
+        y = y + self.rows
+    end
+
+    local pixelLocation = x + (y * self.cols)
+    self.display[pixelLocation + 1] = not self.display[pixelLocation + 1]
+
+    return not self.display[pixelLocation + 1]
+end
+
+function renderer:Render()
+    for i = 1, self.cols * self.rows do
+        local x = (i % self.cols) * self.scale
+        local y = math.floor(i / self.cols) * self.scale
+        
+        if self.display[i] == true then
+            love.graphics.rectangle("fill", x, y, self.scale, self.scale)
+        end
+    end
+end
+
+return renderer
