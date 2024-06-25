fe8fb72a14dcaafb62aac45d8daf8139dc204a2e
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-06-25 14:21:35 -0600

    keyboard

---
+++ keyboard.lua
@@ -0,0 +1,44 @@
+local keyboard = {}
+
+keyboard.keymap = {
+    ["1"] = 0x1, 
+    ["2"] = 0x2, 
+    ["3"] = 0x3, 
+    ["4"] = 0xC,
+    ["q"] = 0x4, 
+    ["w"] = 0x5, 
+    ["e"] = 0x6, 
+    ["r"] = 0xD,
+    ["a"] = 0x7, 
+    ["s"] = 0x8, 
+    ["d"] = 0x9, 
+    ["f"] = 0xE,
+    ["z"] = 0xa, 
+    ["x"] = 0x0, 
+    ["c"] = 0xB, 
+    ["v"] = 0xF,
+}
+
+keyboard.keysPressed = {}
+keyboard.onNextKeyPress = nil
+
+function keyboard:IsKeyPressed(keycode)
+    return self.keysPressed[keycode]
+end
+
+function love.keypressed(_, scancode, isRepeat)
+    if isRepeat then return end
+
+    local key = keyboard.keymap[scancode]
+    keyboard.keysPressed[key] = true
+    
+    if keyboard.onNextKeyPress ~= nil and key ~= nil then
+        keyboard.onNextKeyPress(key)
+        keyboard.onNextKeyPress = nil
+    end
+end
+
+function love.keyreleased(_, scancode)
+    local key = keyboard.keymap[scancode]
+    keyboard.keysPressed[key] = false
+end
